699c89864ea2348c60c6601a8048f27977cd70bf
aliibnemasud <aliibnemasud@gmail.com>
2022-05-09 15:41:47 +0600

    two section added

--- index.js
+++ index.js
@@ -16,29 +16,34 @@ app.get("/services", (req, res) => {
     res.send("Services...");
 })
 
-app.get('/blogs', (req, res) => {
-    res.send("Blog page")
-})
+
 
 // Database connection
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xenpn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
-
-
 // load all data
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db('cycleStore').collection('products');
+        const blogCollection = client.db('cycleStore').collection('blogs');
 
+        // Products
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        // Blogs 
+        app.get('/blogs', async (req, res) => {
+            const query = {};
+            const cursor = blogCollection.find(query);
+            const blogs = await cursor.toArray();
+            res.send(blogs);
         });
 
         // Find product by id
